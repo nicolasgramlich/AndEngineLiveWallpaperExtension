6fc0dfdae1472dafbaed318e262e8a9ff84f0943
Nicolas Gramlich <nicolasgramlich@gmail.com>
2010-03-14 16:12:30 +0100

    Modifications so simplicity of extenders is improvd.

--- src/org/anddev/andengine/ui/livewallpaper/BaseWallpaperService.java
+++ src/org/anddev/andengine/ui/livewallpaper/BaseWallpaperService.java
@@ -5,11 +5,14 @@ import net.rbgrn.opengl.GLWallpaperService;
 import org.anddev.andengine.engine.EngineOptions;
 import org.anddev.andengine.entity.Scene;
 import org.anddev.andengine.opengl.view.RenderSurfaceView;
-import org.anddev.andengine.sensor.accelerometer.AccelerometerListener;
+import org.anddev.andengine.sensor.accelerometer.IAccelerometerListener;
+import org.anddev.andengine.ui.IGameInterface;
+import org.anddev.andengine.util.Debug;
 
+import android.content.Intent;
 import android.opengl.GLSurfaceView.Renderer;
 
-public abstract class BaseWallpaperService extends GLWallpaperService {
+public abstract class BaseWallpaperService extends GLWallpaperService implements IGameInterface {
 	// ===========================================================
 	// Constants
 	// ===========================================================
@@ -26,46 +29,65 @@ public abstract class BaseWallpaperService extends GLWallpaperService {
 
 	@Override
 	public void onCreate() {
+		Debug.d("onCreate");
 		super.onCreate();
 		
 		this.mEngine = this.onLoadEngine();
 		applyEngineOptions(this.mEngine.getEngineOptions());
 
 		this.onLoadResources();
-		this.mEngine.setScene(this.onLoadScene());
+		final Scene scene = this.onLoadScene();
+		this.mEngine.onLoadComplete(scene);
 		this.onLoadComplete();
+		this.mEngine.start();
+	}
+	
+	@Override
+	public void onRebind(Intent pIntent) {
+		Debug.d("onRebind");
+		super.onRebind(pIntent);
+	}
+	
+	@Override
+	public boolean onUnbind(Intent pIntent) {
+		Debug.d("onUnbind");
+		return super.onUnbind(pIntent);
+	}
+	
+	@Override
+	public void onDestroy() {
+		Debug.d("onDestroy");
+		super.onDestroy();
 	}
 
 	// ===========================================================
 	// Getter & Setter
 	// ===========================================================
+	
+	public org.anddev.andengine.engine.Engine getEngine() {
+		return this.mEngine;
+	}
 
 	// ===========================================================
 	// Methods for/from SuperClass/Interfaces
 	// ===========================================================
 
-	protected abstract Scene onLoadScene();
-
-	protected abstract void onLoadResources();
-
-	protected abstract void onLoadComplete();
-
-	protected abstract org.anddev.andengine.engine.Engine onLoadEngine();
+	protected abstract void onPause();
 
 	@Override
 	public Engine onCreateEngine() {
-		return new WallpaperEngine();
+		return new BaseWallpaperEngine();
 	}
 
 	// ===========================================================
 	// Methods
 	// ===========================================================
 	
-	private void applyEngineOptions(EngineOptions pEngineOptions) {
+	private void applyEngineOptions(final EngineOptions pEngineOptions) {
 		
 	}
 
-	protected void enableAccelerometer(final AccelerometerListener pAccelerometerListener) {
+	protected void enableAccelerometer(final IAccelerometerListener pAccelerometerListener) {
 		this.mEngine.enableAccelerometer(this, pAccelerometerListener);
 	}
 
@@ -73,7 +95,7 @@ public abstract class BaseWallpaperService extends GLWallpaperService {
 	// Inner and Anonymous Classes
 	// ===========================================================
 
-	class WallpaperEngine extends GLEngine {
+	class BaseWallpaperEngine extends GLEngine {
 		// ===========================================================
 		// Fields
 		// ===========================================================
@@ -84,8 +106,7 @@ public abstract class BaseWallpaperService extends GLWallpaperService {
 		// Constructors
 		// ===========================================================
 
-		public WallpaperEngine() {
-			super();
+		public BaseWallpaperEngine() {
 			this.mRenderer = new RenderSurfaceView.Renderer(BaseWallpaperService.this.mEngine);
 			this.setRenderer(this.mRenderer);
 			this.setRenderMode(RENDERMODE_CONTINUOUSLY);
@@ -94,6 +115,18 @@ public abstract class BaseWallpaperService extends GLWallpaperService {
 		// ===========================================================
 		// Methods for/from SuperClass/Interfaces
 		// ===========================================================
+		
+		@Override
+		public void onResume() {
+			super.onResume();
+			BaseWallpaperService.this.getEngine().reloadTextures();
+		}
+		
+		@Override
+		public void onPause() {
+			super.onPause();
+			BaseWallpaperService.this.onPause();
+		}
 
 		@Override
 		public void onDestroy() {
